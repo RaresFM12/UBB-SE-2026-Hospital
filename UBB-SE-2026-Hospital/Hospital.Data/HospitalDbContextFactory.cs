42d501f7d4a32636ec0f5819fb3421f1bb39274c
tiberiafarkas <farcastiberia@yahoo.com>
2026-06-06 01:20:19 +0300

    Update HospitalDbContextFactory.cs

--- UBB-SE-2026-Hospital/Hospital.Data/HospitalDbContextFactory.cs
+++ UBB-SE-2026-Hospital/Hospital.Data/HospitalDbContextFactory.cs
@@ -1,54 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using System;
-using System.IO;
-using System.Text.Json;
 
 namespace Hospital.Data;
 
 public class HospitalDbContextFactory : IDesignTimeDbContextFactory<HospitalDbContext>
 {
-    private const string FallbackConnectionString = "Server=localhost\\SQLEXPRESS;Database=HospitalDatabase;Trusted_Connection=True;TrustServerCertificate=True;";
+    private const string LocalDbConnectionString = "Server=localhost\\SQLEXPRESS;Database=HospitalDatabase;Trusted_Connection=True;TrustServerCertificate=True;";
 
-    public HospitalDbContext CreateDbContext(string[] arguments)
+    public HospitalDbContext CreateDbContext(string[] args)
     {
-        string connectionString = null;
-        try
-        {
-            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Hospital.API");
-            if (!Directory.Exists(basePath))
-            {
-                basePath = Directory.GetCurrentDirectory();
-            }
-            var configPath = Path.Combine(basePath, "appsettings.json");
-            if (File.Exists(configPath))
-            {
-                var jsonContent = File.ReadAllText(configPath);
-                var jsonDocumentOptions = new JsonDocumentOptions
-                {
-                    CommentHandling = JsonCommentHandling.Skip
-                };
-                using (var jsonDocument = JsonDocument.Parse(jsonContent, jsonDocumentOptions))
-                {
-                    if (jsonDocument.RootElement.TryGetProperty("ConnectionStrings", out var connectionStringsSection) &&
-                        connectionStringsSection.TryGetProperty("DefaultConnection", out var defaultConnectionProperty))
-                    {
-                        connectionString = defaultConnectionProperty.GetString();
-                    }
-                }
-            }
-        }
-        catch (Exception exception)
-        {
-            Console.WriteLine($"Error reading connection string from appsettings: {exception.Message}");
-        }
-
-        connectionString ??= FallbackConnectionString;
-
-        var dbContextOptionsBuilder = new DbContextOptionsBuilder<HospitalDbContext>()
-            .UseSqlServer(connectionString)
+        var options = new DbContextOptionsBuilder<HospitalDbContext>()
+            .UseSqlServer(LocalDbConnectionString)
             .Options;
 
-        return new HospitalDbContext(dbContextOptionsBuilder);
+        return new HospitalDbContext(options);
     }
 }
